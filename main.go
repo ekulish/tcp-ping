e28bbd96d5b102875c743a7855bf15db8bf1499e
Evgeniy Kulish <ekulish@gmail.com>
2014-06-24 20:49:37 -0700

    command line agruments added, possibility to print result or save it to a file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -9,42 +10,67 @@ import (
 	"time"
 )
 
+// define flags
+var flagHost string
+var flagPort int
+var flagTimeout int
+var flagPeriod int
+var flagFile string
+
 var logFile *os.File
 
 func printLog(message string) {
-	fmt.Fprintln(logFile, time.Now().Format("2006-01-02 15:04:05"), message)
+	if flagFile != "" {
+		fmt.Fprintln(logFile, time.Now().Format("2006-01-02 15:04:05"), message)
+	} else {
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), message)
+	}
+}
+
+func init() {
+	flag.StringVar(&flagHost, "h", "127.0.0.1", "Host name or IP address")
+	flag.IntVar(&flagPort, "p", 80, "Host port")
+	flag.IntVar(&flagTimeout, "t", 10, "Connection timeout (in seconds)")
+	flag.IntVar(&flagPeriod, "r", 60, "Dial period (in seconds)")
+	flag.StringVar(&flagFile, "f", "", "File name which is used instead of printing results")
 }
 
 func main() {
-	//conn, err := tls.Dial("tcp", "172.17.0.191:9000", &config)
-	var err error
-	logFile, err = os.Create("ping_log.txt")
-	defer logFile.Close()
+	flag.Parse()
 
-	if err != nil {
-		log.Fatal(err)
-		return
+	var err error
+
+	// create/open file log, if file name presents as a command line argument
+	if flagFile != "" {
+		logFile, err = os.Create(flagFile)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		defer logFile.Close()
 	}
+
 	c := make(chan string)
 
+	host := fmt.Sprintf("%v:%v", flagHost, flagPort)
+
+	// execute dail in separate routine to the defined host:port until program is killed
 	for {
 		go func() {
-			//conn, err := net.DialTimeout("tcp", "172.17.0.191:9000", time.Duration(10*time.Second))
-			//198.199.122.230
-			conn, err := net.DialTimeout("tcp", "198.199.122.230:22", time.Duration(10*time.Second))
-			defer conn.Close()
-
+			conn, err := net.DialTimeout("tcp", host, time.Duration(flagTimeout)*time.Second)
 			if err != nil {
 				// log error
 				c <- fmt.Sprint("Error: ", err)
 				return
 			}
+			defer conn.Close()
+
 			// log success
 			c <- fmt.Sprint("Connected to ", conn.RemoteAddr())
 		}()
 
 		printLog(<-c)
 
-		time.Sleep(time.Second * 60)
+		time.Sleep(time.Duration(flagPeriod) * time.Second)
 	}
 }
